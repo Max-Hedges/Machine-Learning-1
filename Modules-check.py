d1f0587c2e9fa99ba94fd4cd5c66751959a86fd2
Max-Hedges <59054610+Max-Hedges@users.noreply.github.com>
2019-12-19 13:28:23 +0000

    Add files via upload

---
+++ Modules-check.py
@@ -0,0 +1,17 @@
+import sys
+import scipy
+import numpy
+import matplotlib
+import pandas
+import sklearn
+
+
+print("Python: {}".format(sys.version))
+print("Scipy: {}".format(scipy.__version__))
+print("Numpy: {}".format(numpy.__version__))
+print("Matplotlib: {}".format(matplotlib.__version__))
+print("Pandas: {}".format(pandas.__version__))
+print("SKLearn: {}".format(sklearn.__version__))
+#All modules up to date on home PC
+#Not yet up to date on Laptop
+
